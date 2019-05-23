7a9b7755ded43fec9ebe96e88c14d03f3cf458c7
Jennifer Currie <renestl@gmail.com>
2019-05-23 16:50:14 -0500

    Initialize Startup

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_startup_project/lib/startup.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_startup_project/lib/startup.rb
@@ -1,5 +1,13 @@
 require "employee"
 
 class Startup
+	attr_reader :name, :funding, :salaries, :employees
+
+	def initialize(name, funding, salaries)
+		@name = name
+		@funding = funding
+		@salaries = salaries
+		@employees = []
+	end
 
 end

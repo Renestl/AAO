031415b15f7dd0cdb9edc09ae9e790246d9ad950
Jennifer Currie <renestl@gmail.com>
2019-05-23 16:51:00 -0500

    Complete Startup project

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_startup_project/lib/startup.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_startup_project/lib/startup.rb
@@ -10,4 +10,68 @@ class Startup
 		@employees = []
 	end
 
+	def valid_title?(title)
+ 		@salaries.has_key?(title)
+	end
+
+	def >(other_startup)
+		return true if self.funding > other_startup.funding
+		false
+	end
+
+	def hire(employee_name, title)
+		if self.valid_title?(title)
+			@employees << Employee.new(employee_name, title)
+		else
+			raise 'This title is invalid.'
+		end
+	end
+
+	def size
+		return @employees.length
+	end
+
+	def pay_employee(employee)
+		payment = @salaries[employee.title]
+
+		if @funding >= payment
+			employee.pay(payment)
+			@funding -= payment			
+		else
+			raise 'There is not enough funding.'
+		end
+	end
+	
+	def payday
+		@employees.map{ |employee| self.pay_employee(employee) }
+	end
+
+	def average_salary
+		total = 0
+
+		self.employees.each do |employee|
+			total += salaries[employee.title]
+		end
+
+		total / self.size
+	end
+
+	def close
+		@employees = []
+		@funding = 0
+	end
+
+	def acquire(another_startup)
+		@funding += another_startup.funding
+		
+		if !@salaries.has_key?(another_startup.salaries)
+			@salaries.merge!(another_startup.salaries) { |salary, v1, v2| v1}
+		end
+
+		self.employees.concat(another_startup.employees)
+
+		another_startup.close
+		
+	end
+
 end

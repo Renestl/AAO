d6add390089ea0a686b6d1b35b73e3929193e854
Jennifer Currie <renestl@gmail.com>
2019-05-23 00:33:07 -0500

    Complete hangman project

--- 2 Software Engineering Foundations/8 Input_Output/Projects/jennifer_currie_hangman_project/lib/hangman.rb
+++ 2 Software Engineering Foundations/8 Input_Output/Projects/jennifer_currie_hangman_project/lib/hangman.rb
@@ -1,5 +1,5 @@
 class Hangman
-  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza", "truck", "sleep", "begin", "grant", "hobbies"]
 
   def self.random_word
     DICTIONARY.sample
@@ -7,7 +7,7 @@ class Hangman
 
   def initialize
     @secret_word = Hangman.random_word
-    @guess_word = Array.new(@secret_word.length) { |ele| ele = "_"}
+    @guess_word = Array.new(@secret_word.length, "_")
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
@@ -25,11 +25,7 @@ class Hangman
   end
 
   def already_attempted?(char)
-    if attempted_chars.include?(char)
-      true
-    else
-      false
-    end
+    attempted_chars.include?(char)
   end
 
   def get_matching_indices(char)
@@ -48,5 +44,52 @@ class Hangman
     indices.each do |idx|
       @guess_word[idx] = char
     end
+  end
+
+  def try_guess(char)
+
+    if already_attempted?(char)
+      puts 'that has already been attempted'
+      return false
+    end 
+
+    attempted_chars << char
+
+    matching = self.get_matching_indices(char)
+    self.fill_indices(char, matching)
+
+    
+    @remaining_incorrect_guesses -= 1 if matching.empty?
+    
+    true
+  end
+
+  def ask_user_for_guess
+    puts 'Enter a char:'
+    self.try_guess(gets.chomp)
+  end
+
+  def win?
+    if @guess_word.join == @secret_word
+      puts "WIN"
+      return true
+    end
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "LOSE"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    if self.win? || self.lose?
+      puts @secret_word
+      return true
+    end
+    false
   end
 end

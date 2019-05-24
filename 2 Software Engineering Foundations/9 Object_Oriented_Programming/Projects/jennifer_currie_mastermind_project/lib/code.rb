e5602cf7d7ba291e4803672845a08a9f4a152640
Jennifer Currie <renestl@gmail.com>
2019-05-24 01:18:49 -0500

    Integrate initialize, pegs and random functions

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_mastermind_project/lib/code.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_mastermind_project/lib/code.rb
@@ -6,9 +6,25 @@ class Code
     "Y" => :yellow
   }
 
+  attr_reader :pegs
+
   def self.valid_pegs?(char_array)
     char_array.all? do |char|
-      POSSIBLE_PEGS.include?(char.upcase)
+      POSSIBLE_PEGS.has_key?(char.upcase)
+    end
+  end
+
+  def self.random(length)
+    # random_pegs = []
+    # length.times { random_pegs << POSSIBLE_PEGS.keys.sample}
+
+    Code.new(Array.new(length) { POSSIBLE_PEGS.keys.sample })
+  end
+  def initialize(pegs)
+    if !Code.valid_pegs?(pegs)
+      raise "Please enter valid peg colors."
+    else
+      @pegs = pegs.map(&:upcase)
     end
   end
 

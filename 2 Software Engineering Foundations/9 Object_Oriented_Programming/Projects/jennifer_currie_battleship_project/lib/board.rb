b2a6ad9be1b57242ffecf25c9ef6f1cd264be2e1
Jennifer Currie <renestl@gmail.com>
2019-05-29 02:29:47 -0500

    Return element at given position

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_battleship_project/lib/board.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_battleship_project/lib/board.rb
@@ -5,5 +5,13 @@ class Board
 		@grid = Array.new(n) {Array.new(n, :N)}
 		@size = n * n
 	end
-  
+
+	def [](board_position)
+		row = board_position[0]
+		col = board_position[1]
+
+		@grid[row][col]
+	end
+	
+
 end

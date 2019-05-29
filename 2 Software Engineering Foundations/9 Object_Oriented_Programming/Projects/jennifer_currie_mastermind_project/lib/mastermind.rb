db0c677dddfbb4ed72a75bbd5a03649003851518
Jennifer Currie <renestl@gmail.com>
2019-05-28 22:16:38 -0500

    Print exact and near matches

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_mastermind_project/lib/mastermind.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_mastermind_project/lib/mastermind.rb
@@ -5,5 +5,10 @@ class Mastermind
 		@secret_code = Code.random(length)
 	end
 
+	def print_matches(code_inst)
+		puts @secret_code.num_exact_matches(code_inst)
+		puts @secret_code.num_near_matches(code_inst)	
+	end
+
 
 end

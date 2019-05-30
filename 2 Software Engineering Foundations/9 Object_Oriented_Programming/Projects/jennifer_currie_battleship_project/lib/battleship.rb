ce037b69309f837cc55f2fdd87de14fe022a67dc
Jennifer Currie <renestl@gmail.com>
2019-05-30 07:43:54 -0500

    Initialize battleship board

--- 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_battleship_project/lib/battleship.rb
+++ 2 Software Engineering Foundations/9 Object_Oriented_Programming/Projects/jennifer_currie_battleship_project/lib/battleship.rb
@@ -2,5 +2,13 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+	attr_reader :board, :player
+
+	def initialize (n)
+		@player = Player.new
+		@board = Board.new(n)
+		@remaining_misses = @board.size * 0.5
+	end
+
 
 end

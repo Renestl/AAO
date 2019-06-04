4b2897ee23a478803e61e064a8ddf7ee780cb8d3
Jennifer Currie <renestl@gmail.com>
2019-06-04 14:36:39 -0500

    Extend Array to integrate any and all methods

--- 3 Ruby/Projects/Enumerable/enumerables.rb
+++ 3 Ruby/Projects/Enumerable/enumerables.rb
@@ -38,7 +38,21 @@ class Array
 		new_array
 	end
 
+	# My Any
+	# Write my_any? to return true if any elements of the array satisfy the block and my_all? to return true only if all elements satisfy the block.
+	def my_any?(&prc)
+		self.my_each do |ele|
+			return true if prc.call(ele)
+		end
+		false
+	end
 
+	def my_all?(&prc)
+		self.my_each do |ele|
+			return false if !prc.call(ele)
+		end
+		true
+	end
 end
 
 # #######################
@@ -59,3 +73,11 @@ end
 # a = [1, 2, 3]
 # p a.my_reject { |num| num > 1 } # => [1]
 # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+## My Any
+
+a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true

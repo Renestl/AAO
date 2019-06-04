74ae99114359d9a31d394c01c7a2623d032205bd
Jennifer Currie <renestl@gmail.com>
2019-06-04 04:21:20 -0500

    Extend Array class with my_reject method
    return new array excluding elements that satisfy the block

--- 3 Ruby/Projects/Enumerable/enumerables.rb
+++ 3 Ruby/Projects/Enumerable/enumerables.rb
@@ -1,7 +1,7 @@
 class Array
 
-# My Each
-# Extend the Array class to include a method named my_each that takes a block, calls the block on every element of the array, and returns the original array.
+	# My Each
+	# Extend the Array class to include a method named my_each that takes a block, calls the block on every element of the array, and returns the original array.
 	def my_each(&prc)
 		idx = 0
 
@@ -14,13 +14,25 @@ class Array
 	end
 
 
-# My Select
-# Now extend the Array class to include my_select that takes a block and returns a new array containing only elements that satisfy the block. Use your my_each method!
+	# My Select
+	# Now extend the Array class to include my_select that takes a block and returns a new array containing only elements that satisfy the block. Use your my_each method!
 	def my_select(&prc)
 		new_array = []
 
 		self.my_each do |ele|
 			new_array << ele if prc.call(ele)				
+		end
+
+		new_array
+	end
+
+	# My Reject
+	# Write my_reject to take a block and return a new array excluding elements that satisfy the block.
+	def my_reject(&prc)
+		new_array = []
+
+		self.my_each do |ele|
+			new_array << ele if !prc.call(ele)				
 		end
 
 		new_array
@@ -38,6 +50,12 @@ end
 
 ## My Select
 
-a = [1, 2, 3, 7]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+# a = [1, 2, 3, 7]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+## My Reject
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]

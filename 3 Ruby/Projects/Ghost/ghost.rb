60b1ae2bc150065e4a64dd4755b77ee94ef8a45a
Jennifer Currie <renestl@gmail.com>
2019-06-20 22:46:23 -0500

    Verify player guess is valid letter

--- 3 Ruby/Projects/Ghost/ghost.rb
+++ 3 Ruby/Projects/Ghost/ghost.rb
@@ -2,16 +2,18 @@ require "set"
 require './player.rb'
 
 class GhostGame
-attr_reader :players, :dictionary 
+	ALPHABET = Set.new("a".."z")
+
+	attr_reader :players, :dictionary, :fragment 
 
 	def initialize(*players)
 		words = File.readlines('dictionary.txt').map(&:chomp)
-		@dictionary = Set.new(words)
-		@players = players		
+		# @dictionary = Set.new(words)
+		@players = players
 	end
 
 	def play_round
-
+		@fragment = ""
 	end
 
 	def current_player
@@ -26,12 +28,26 @@ attr_reader :players, :dictionary
 		players.rotate!
 	end
 
-	def take_turn(player)
+	def take_turn
+		puts "It's #{current_player.name}'s turn!'"
+		letter = current_player.guess
 
+		if valid_play?(letter)
+			puts letter
+		else
+			current_player.alert_invalid_guess
+			take_turn
+		end
 	end
 
 	def valid_play?(string)
-
+		if ALPHABET.include?(string)
+			return true
+		else
+			return false
+		end
 	end
 
 end
+
+# game = GhostGame.new(Player.new("Rob"),Player.new("Janice"), Player.new("Rick"),Player.new("Joy"))

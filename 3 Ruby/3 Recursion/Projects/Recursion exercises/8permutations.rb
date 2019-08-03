9a9ec6bb90d6c7240e770cfb9f51e80abb6bc3dc
Jennifer Currie <renestl@gmail.com>
2019-08-03 05:06:52 -0500

    Complete permutations exercise

---
+++ 3 Ruby/3 Recursion/Projects/Recursion exercises/8permutations.rb
@@ -0,0 +1,33 @@
+# Permutations
+# Write a recursive method permutations(array) that calculates all the permutations of the given array. For an array of length n there are n! different permutations. So for an array with three elements we will have 3 * 2 * 1 = 6 different permutations.
+
+# permutations([1, 2, 3]) # => [[1, 2, 3], [1, 3, 2],
+#                         #     [2, 1, 3], [2, 3, 1],
+#                         #     [3, 1, 2], [3, 2, 1]]
+# You can use Ruby's built in Array#permutation method to get a better understanding of what you will be building.
+
+# [1, 2, 3].permutation.to_a  # => [[1, 2, 3], [1, 3, 2],
+#                             #     [2, 1, 3], [2, 3, 1],
+#                             #     [3, 1, 2], [3, 2, 1]]
+
+def permutations(array)
+	return [array] if array.length <= 1
+
+	first = array.shift
+	perms = permutations(array)
+	total_permutations = []
+
+	perms.each do |perm|
+		(0..perm.length).each do |i|
+			total_permutations << perm[0...i] + [first] + perm[i..-1]
+		end
+	end
+
+	total_permutations
+end
+
+# Test cases
+p permutations([1, 2, 3]) # => [[1, 2, 3], [1, 3, 2],
+#                         #     [2, 1, 3], [2, 3, 1],
+#                         #     [3, 1, 2], [3, 2, 1]]
+# p permutations([5, 6, 7, 8, 9])
---
+++ 3 Ruby/3 Recursion/Projects/Recursion exercises/9make_change.rb
@@ -0,0 +1,24 @@
+# Make Change
+# RubyQuiz: [Make change](http://web.archive.org/web/20130215052843/http://rubyquiz.com/quiz154.html).
+
+# Here's a game plan for solving the problem:
+
+# First, write a 'greedy' version called greedy_make_change:
+
+# Take as many of the biggest coin as possible and add them to your result.
+# Add to the result by recursively calling your method on the remaining amount, leaving out the biggest coin, until the remainder is zero.
+# Once you have a working greedy version, talk with your partner about refactoring this to make_better_change. What's wrong with greedy_make_change?
+
+# Consider the case of greedy_make_change(24, [10,7,1]). Because it takes as many 10 pieces as possible, greedy_make_change misses the correct answer of [10,7,7] (try it in pry).
+
+# To make_better_change, we only take one coin at a time and never rule out denominations that we've already used. This allows each coin to be available each time we get a new remainder. By iterating over the denominations and continuing to search for the best change, we assure that we test for 'non-greedy' uses of each denomination.
+
+# Discuss the following game plan and then work together to implement your new method:
+
+# Iterate over each coin.
+# Grab only one of that one coin and recursively call make_better_change on the remainder using coins less than or equal to the current coin.
+# Add the the single coin to the change returned by the recursive call. This will be a possible solution, but maybe not the best one.
+# Keep track of the best solution and return it at the end.
+# N.B. Don't generate every possible permutation of coins and then compare them. Remember that a permutation is not the same thing as a combination - we will need to check every combination of coins that add up to our target, we just don't want to check the same combination in different orders. If you get stuck you can start by writing a solution that calculates and compares all of the permutations without storing them in an array. Then go back and refactor your solution so that it only calculates and compares all of the different combinations. If you and your partner get totally stuck, or would like to compare your answer, feel free to reference this [video walkthrough](https://vimeo.com/91207646) of the problem.
+
+# Make sure you and your partner understand each line before moving on.

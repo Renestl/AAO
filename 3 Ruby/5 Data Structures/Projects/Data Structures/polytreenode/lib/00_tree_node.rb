f4a469019f0a0cdbefddd1897f5e96607312a216
Jennifer Currie <renestl@gmail.com>
2019-08-27 19:59:03 -0500

    Complete up to phase3

--- 3 Ruby/5 Data Structures/Projects/Data Structures/polytreenode/lib/00_tree_node.rb
+++ 3 Ruby/5 Data Structures/Projects/Data Structures/polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
+	attr_reader :value, :parent, :children
+
+	def initialize(value)
+		@value = value
+		@parent = nil
+		@children = []
+	end
+
+	def parent=(node)
+		if node.nil?
+			@parent = nil
+		else
+			if self.parent
+				self.parent.children.reject! { |child| child == self }
+			end
+
+			@parent = node
+			node.children << self unless node.children.include?(self)
+		end
+
+		def add_child(child_node)
+			child_node.parent = self
+		end
+
+		def remove_child(child_node)
+			raise "This node is not a child" if child_node.parent != self
+			child_node.parent = nil
+		end
+	end
 
 end

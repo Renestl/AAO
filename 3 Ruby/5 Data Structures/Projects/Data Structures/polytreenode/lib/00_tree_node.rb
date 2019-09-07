8d598cda99bdca41a6ee80c25677acd4abd6dd65
Jennifer Currie <renestl@gmail.com>
2019-09-06 21:36:13 -0500

    PolyTreeNode exercise

--- 3 Ruby/5 Data Structures/Projects/Data Structures/polytreenode/lib/00_tree_node.rb
+++ 3 Ruby/5 Data Structures/Projects/Data Structures/polytreenode/lib/00_tree_node.rb
@@ -17,16 +17,39 @@ class PolyTreeNode
 
 			@parent = node
 			node.children << self unless node.children.include?(self)
-		end
-
-		def add_child(child_node)
-			child_node.parent = self
-		end
-
-		def remove_child(child_node)
-			raise "This node is not a child" if child_node.parent != self
-			child_node.parent = nil
-		end
+		end	
 	end
 
+	def add_child(child_node)
+		child_node.parent = self
+	end
+
+	def remove_child(child_node)
+		raise "This node is not a child" if child_node.parent != self
+		child_node.parent = nil
+	end
+
+	def dfs(target_value)
+		return self if self.value == target_value
+
+		children.each do |child|
+			result = child.dfs(target_value)
+			return result if result
+		end
+
+		nil
+	end
+
+	def bfs(target_value)
+		bfs_queue = []
+		bfs_queue << self
+
+		until bfs_queue.empty?
+			first_node = bfs_queue.shift
+
+			return first_node if first_node.value == target_value
+
+			bfs_queue.concat(first_node.children)
+		end
+	end
 end

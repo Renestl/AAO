9354a67f2428140a231f44c80581c28179042527
Jennifer Currie <renestl@gmail.com>
2019-10-16 04:07:48 -0500

    Phase 2

--- 3 Ruby/5 Data Structures/Projects/Data Structures/Knights_Travails/knights.rb
+++ 3 Ruby/5 Data Structures/Projects/Data Structures/Knights_Travails/knights.rb
@@ -1,6 +1,8 @@
 require '../polytreenode/lib/00_tree_node.rb'
 
 class KnightPathFinder
+	attr_reader :start_pos
+
 	POSITIONS = [[-1,-1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
 	def self.valid_moves(pos)
@@ -22,12 +24,39 @@ class KnightPathFinder
 	def initialize(start_pos)
 		@start_pos = start_pos
 		@considered_positions = [start_pos]
+
+		build_move_tree
 	end
 	
 	def new_move_positions(pos)
 		KnightPathFinder.valid_moves(pos)
 			.reject { |move| @considered_positions.include?(move)}
 			.each { |move| @considered_positions << move }
+	end
+
+	# private
+
+  attr_accessor :root_node, :considered_positions
+
+	def build_move_tree
+		self.root_node = PolyTreeNode.new(start_pos)
+
+		node_queue = [root_node]
+
+		until node_queue.empty?
+			current_node = node_queue.shift
+
+			current_pos = current_node.value
+
+			new_move_positions(current_pos).each do | next_pos|
+				p next_node = PolyTreeNode.new(next_pos)
+				current_node.add_child(next_node)
+				node_queue << next_node
+			end
+		end
+	end
+
+	def find_path
 	end
 
 end
